ed0f62be9e531ca70db218d49df9472f6ba901ef
m-star18 <masutangoribura@gmail.com>
2023-05-25 16:18:49 +0900

    add job script

--- download_dataset_job/download_weights_kitti.sh
+++ download_dataset_job/download_weights_kitti.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 DATADIR="/groups/gcd50654/tier4/neural-scene-graphs/example_weights"
-TEMPFILE="/groups/gcd50654/tier4/neural-scene-graphs/tmp.zip"
+TEMPFILE="/groups/gcd50654/tier4/neural-scene-graphs/example_weights/tmp.zip"
 wget -O $TEMPFILE "https://drive.google.com/uc?export=download&id=1o28o6gOGHrjQ3LA5Kazj6zdzXEVboS8g" && unzip $TEMPFILE -d $DATADIR && rm -f $TEMPFILE
--- render_nerf.sh
+++
@@ -1,1 +0,0 @@
-PYTHONPATH=. python main.py --config example_configs/config_kitti_0006_example_render.txt
